2a91ce9914c0fcec10a9a89b9897fe765f421eca
sususweet <tyq9702@sina.com>
2025-11-14 21:14:11 +0800

    fix: add entities for T0xB6. Ref #45.

--- custom_components/midea_auto_cloud/device_mapping/T0xB6.py
+++ custom_components/midea_auto_cloud/device_mapping/T0xB6.py
@@ -1,4 +1,5 @@
-from homeassistant.const import Platform
+from homeassistant.components.sensor import SensorDeviceClass, SensorStateClass
+from homeassistant.const import Platform, UnitOfPower, UnitOfElectricPotential
 from homeassistant.components.switch import SwitchDeviceClass
 
 DEVICE_MAPPING = {
@@ -6,6 +7,14 @@ DEVICE_MAPPING = {
         "rationale": ["off", "on"],
         "queries": [{}],
         "centralized": [],
+        "calculate": {
+            "get": [
+                {
+                    "lvalue": "[b7_vbattery]",
+                    "rvalue": "[b7_vbatt] / 1000.0"
+                },
+            ],
+        },
         "entities": {
             Platform.SWITCH: {
                 "power": {
@@ -18,6 +27,25 @@ DEVICE_MAPPING = {
                     "device_class": SwitchDeviceClass.SWITCH,
                 }
             },
+            Platform.SENSOR: {
+                "error_code": {
+                    "device_class": SensorDeviceClass.ENUM
+                },
+                "b7_left_status": {
+                    "device_class": SensorDeviceClass.ENUM,
+                    "translation_key": "left_status",
+                },
+                "b7_right_status": {
+                    "device_class": SensorDeviceClass.ENUM,
+                    "translation_key": "right_status",
+                },
+                "b7_vbattery":{
+                    "device_class": SensorDeviceClass.VOLTAGE,
+                    "unit_of_measurement": UnitOfElectricPotential.VOLT,
+                    "state_class": SensorStateClass.MEASUREMENT,
+                    "translation_key": "battery_voltage",
+                }
+            },
             Platform.SELECT: {
                 "wind_pressure": {
                     "options": {
@@ -26,6 +54,15 @@ DEVICE_MAPPING = {
                         "medium": {"wind_pressure": "2"},
                         "high": {"wind_pressure": "3"},
                         "extreme": {"wind_pressure": "4"},
+                    }
+                },
+                "gear": {
+                    "options": {
+                        "off": {"gear": 0},
+                        "low": {"gear": 1},
+                        "medium": {"gear": 2},
+                        "high": {"gear": 3},
+                        "extreme": {"gear": 4},
                     }
                 },
             },
