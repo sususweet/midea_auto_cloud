ed4e3d34feadaa6661ae12f45ab9c6e0566dfc8b
Cyborg2017 <wangyang0987654@gmail.com>
2025-12-26 20:58:41 +0800

    feat: add device mapping for T0xFB(570667EC)
    * Device type: T0xFB, Sn8: 570667EC, model: HDU20WS.

--- custom_components/midea_auto_cloud/device_mapping/T0xFB.py
+++ custom_components/midea_auto_cloud/device_mapping/T0xFB.py
@@ -1,5 +1,5 @@
 from homeassistant.const import Platform, UnitOfTemperature, UnitOfVolume, UnitOfTime, PERCENTAGE, PRECISION_HALVES, \
-    UnitOfEnergy, UnitOfPower
+    PRECISION_WHOLE, UnitOfEnergy, UnitOfPower
 from homeassistant.components.sensor import SensorStateClass, SensorDeviceClass
 from homeassistant.components.binary_sensor import BinarySensorDeviceClass
 from homeassistant.components.switch import SwitchDeviceClass
@@ -39,6 +39,65 @@ DEVICE_MAPPING = {
                     "options": {
                         "low": {"gear": 1},
                         "high": {"gear": 3}
+                    }
+                }
+            },
+            Platform.SENSOR: {
+                "power_statistics": {
+                    "device_class": SensorDeviceClass.POWER,
+                    "unit_of_measurement": UnitOfPower.WATT,
+                    "state_class": SensorStateClass.MEASUREMENT
+                }
+            }
+        }
+    },
+    "570667EC": {
+        "rationale": ["off", "on"],
+        "queries": [{}],
+        "centralized": [],
+        "entities": {
+            Platform.SWITCH: {
+                "auto_power_off": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                },
+                "humidification": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "rationale": ['off', 'no_change'],
+                },
+                "lock": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "translation_key": "child_lock",
+                },
+                "screen_close": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "rationale": ['on', 'off'],
+                },
+                "voice": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "rationale": ['close_buzzer', 'open_buzzer'],
+                }
+            },
+            Platform.CLIMATE: {
+                "electric_heater": {
+                    "power": "power",
+                    "hvac_modes": {
+                        "off": {"power": "off"},
+                        "heat": {"power": "on"}
+                    },
+                    "target_temperature": "temperature",
+                    "current_temperature": "cur_temperature",
+                    "min_temp": 5,
+                    "max_temp": 35,
+                    "temperature_unit": UnitOfTemperature.CELSIUS,
+                    "precision": PRECISION_WHOLE,
+                }
+            },
+            Platform.SELECT: {
+                "gear": {
+                    "options": {
+                        "left_warm": {"gear": 1},
+                        "right_warm": {"gear": 2},
+                        "full_on": {"gear": 3}
                     }
                 }
             },
