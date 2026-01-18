023243297b1c94f5e386e1cc842dd7a7057536b8
Cyborg2017 <wangyang0987654@gmail.com>
2026-01-18 23:30:38 +0800

    fix: Correct return type from string to number for T0xD9

--- custom_components/midea_auto_cloud/device_mapping/T0xD9.py
+++ custom_components/midea_auto_cloud/device_mapping/T0xD9.py
@@ -57,70 +57,70 @@ DEVICE_MAPPING = {
                 "db_program": {
                     "options": {
                         "baby_clothes": {"db_program": "baby_clothes"},
-                        "baby_clothes_dry": {"db_program": "151"},
+                        "baby_clothes_dry": {"db_program": 151},
                         "clean_stains": {"db_program": "clean_stains"},
                         "cold_wash": {"db_program": "cold_wash"},
                         "cook_wash": {"db_program": "cook_wash"},
-                        "fast_wash": {"db_program": "137"},
-                        "hot_wind_dry": {"db_program": "153"},
+                        "fast_wash": {"db_program": 137},
+                        "hot_wind_dry": {"db_program": 153},
                         "rinsing_dehydration": {"db_program": "rinsing_dehydration"},
                         "self_wash_5": {"db_program": "self_wash_5"},
                         "single_dehytration": {"db_program": "single_dehytration"},
                         "single_drying": {"db_program": "single_drying"},
-                        "small_wash_dry": {"db_program": "138"},
-                        "socks": {"db_program": "148"},
+                        "small_wash_dry": {"db_program": 138},
+                        "socks": {"db_program": 148},
                         "standard": {"db_program": "standard"},
-                        "underpants": {"db_program": "156"},
+                        "underpants": {"db_program": 156},
                         "underwear": {"db_program": "underwear"},
                         "water_ssp": {"db_program": "water_ssp"}
                    }
                 },
                 "db_temperature": {
                     "options": {
-                        "冷水": {"db_temperature": "0"},
-                        "30℃": {"db_temperature": "3"},
-                        "40℃": {"db_temperature": "4"},
-                        "60℃": {"db_temperature": "5"},
-                        "95℃": {"db_temperature": "6"}
+                        "冷水": {"db_temperature": 0},
+                        "30℃": {"db_temperature": 3},
+                        "40℃": {"db_temperature": 4},
+                        "60℃": {"db_temperature": 5},
+                        "95℃": {"db_temperature": 6}
                     },
                     "translation_key": "temperature"
                 },
                 "db_detergent": {
                     "options": {
-                        "关闭": {"db_detergent": "0"},
-                        "L1": {"db_detergent": "1"},
-                        "L2": {"db_detergent": "2"},
-                        "L3": {"db_detergent": "3"}
+                        "关闭": {"db_detergent": 0},
+                        "L1": {"db_detergent": 1},
+                        "L2": {"db_detergent": 2},
+                        "L3": {"db_detergent": 3}
                     },
                     "translation_key": "detergent"
                 },
                 "db_dehydration_speed": {
                     "options": {
-                        "免脱水": {"db_dehydration_speed": "0"},
-                        "800转": {"db_dehydration_speed": "3"},
-                        "1000转": {"db_dehydration_speed": "4"}
+                        "免脱水": {"db_dehydration_speed": 0},
+                        "800转": {"db_dehydration_speed": 3},
+                        "1000转": {"db_dehydration_speed": 4}
                     },
                     "translation_key": "dehydration_speed"
                 },
                 "db_rinse_count": {
                     "options": {
-                        "1次": {"db_rinse_count": "1"},
-                        "2次": {"db_rinse_count": "2"},
-                        "3次": {"db_rinse_count": "3"},
-                        "4次": {"db_rinse_count": "4"},
-                        "5次": {"db_rinse_count": "5"}
+                        "1次": {"db_rinse_count": 1},
+                        "2次": {"db_rinse_count": 2},
+                        "3次": {"db_rinse_count": 3},
+                        "4次": {"db_rinse_count": 4},
+                        "5次": {"db_rinse_count": 5}
                     },
                     "translation_key": "soak_count"
                 },
                 "db_dry": {
                     "options": {
-                        "关闭": {"db_dry": "0"},
-                        "智能": {"db_dry": "1"},
-                        "定时240": {"db_dry": "12"},
-                        "定时180": {"db_dry": "11"},
-                        "定时120": {"db_dry": "7"},
-                        "定时60": {"db_dry": "5"},
-                        "定时30": {"db_dry": "4"}              
+                        "关闭": {"db_dry": 0},
+                        "智能": {"db_dry": 1},
+                        "定时240": {"db_dry": 12},
+                        "定时180": {"db_dry": 11},
+                        "定时120": {"db_dry": 7},
+                        "定时60": {"db_dry": 5},
+                        "定时30": {"db_dry": 4}              
                     }
                 }
             },
