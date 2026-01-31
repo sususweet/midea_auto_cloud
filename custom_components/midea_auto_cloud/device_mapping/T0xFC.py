234b4d6e4b41ec2bd6997bc48487de473693791a
Cyborg2017 <wangyang0987654@gmail.com>
2026-01-31 13:59:09 +0800

    feat: update device mapping for T0xFC(571Z307F)

--- custom_components/midea_auto_cloud/device_mapping/T0xFC.py
+++ custom_components/midea_auto_cloud/device_mapping/T0xFC.py
@@ -200,5 +200,103 @@ DEVICE_MAPPING = {
                 }
             }
         }
+    },
+    "571Z307F": {
+        "rationale": ["off", "on"],
+        "queries": [{}],
+        "centralized": [],
+        "entities": {
+            Platform.NUMBER: {
+                "hosting_upper": {
+                    "min": 10,
+                    "max": 75,
+                    "step": 1
+                },
+                "hosting_lower": {
+                    "min": 10,
+                    "max": 75,
+                    "step": 1
+                }
+            },
+            Platform.SWITCH: {
+                "power": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                },
+                "buzzer": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "translation_key": "voice",
+                },
+                "lock": {
+                    "device_class": SwitchDeviceClass.SWITCH,
+                    "translation_key": "child_lock",
+                },
+                "waterions":{
+                    "device_class": SwitchDeviceClass.SWITCH,
+                }
+            },
+            Platform.SELECT: {
+                "mode": {
+                    "options": {
+                        "manual": {"mode": "manual"},
+                        "sleep": {"mode": "sleep"},
+                        "auto": {"mode": "auto"},
+                        "fast": {"mode": "fast"}
+                    }
+                },
+                "bright": {
+                    "options": {
+                        "全亮": {"bright": 0},
+                        "半亮": {"bright": 6},
+                        "熄灭": {"bright": 7}
+                    }
+                },
+                "gear": {
+                    "options": {
+                        "low": {"wind_speed": 1},
+                        "medium": {"wind_speed": 2},
+                        "high": {"wind_speed": 3}
+                    }
+                },
+                "humidity": {
+                    "options": {
+                        "off": {"humidity": 0},
+                        "40%": {"humidity": 40},
+                        "50%": {"humidity": 50},
+                        "60%": {"humidity": 60}
+                    }
+                },
+            },
+            Platform.SENSOR: {
+                "deep_filter_percent": {
+                    "unit_of_measurement": PERCENTAGE,
+                    "state_class": SensorStateClass.MEASUREMENT
+                },
+                "temperature_feedback": {
+                    "device_class": SensorDeviceClass.TEMPERATURE,
+                    "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                    "state_class": SensorStateClass.MEASUREMENT
+                },
+                "humidify_feedback": {
+                    "device_class": SensorDeviceClass.HUMIDITY,
+                    "unit_of_measurement": "%",
+                    "state_class": SensorStateClass.MEASUREMENT
+                },
+                "ash_tvoc":{
+                    "unit_of_measurement": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+                    "state_class": SensorStateClass.MEASUREMENT,
+                    "translation_key": "tvoc_value"
+                },
+                "pm25":{
+                    "device_class": SensorDeviceClass.PM25,
+                    "unit_of_measurement": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+                    "state_class": SensorStateClass.MEASUREMENT
+                },
+                "pm10":{
+                    "device_class": SensorDeviceClass.PM10,
+                    "unit_of_measurement": CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+                    "state_class": SensorStateClass.MEASUREMENT
+                }
+            }
+        }
     }
 }
