179cf33b19f0b0b6dc22a61b243f4af9180ae15a
sususweet <tyq9702@sina.com>
2026-06-13 01:05:37 +0800

    feat: add support for new T0xC3 device(171000AU). Fix #190.

--- custom_components/midea_auto_cloud/device_mapping/T0xC3.py
+++ custom_components/midea_auto_cloud/device_mapping/T0xC3.py
@@ -1,7 +1,80 @@
 from homeassistant.const import Platform, UnitOfTemperature, PRECISION_HALVES, CONCENTRATION_MICROGRAMS_PER_CUBIC_METER, \
-    CONCENTRATION_PARTS_PER_MILLION
+    CONCENTRATION_PARTS_PER_MILLION, UnitOfEnergy
 from homeassistant.components.sensor import SensorStateClass, SensorDeviceClass
 from homeassistant.components.switch import SwitchDeviceClass
+
+# MSC-200D 2N8 pool / spa heat pump (171000AU, subtype 513).
+_POOL_HEAT_PUMP_MAPPING = {
+    "rationale": ["off", "on"],
+    "queries": [{}, {"query_type": 2}],
+    "centralized": [],
+    "entities": {
+        Platform.CLIMATE: {
+            "Water": {
+                "translation_key": "water",
+                "power": "mode",
+                "hvac_modes": {
+                    "off": {"mode": "off"},
+                    "heat": {"mode": "heat"},
+                    "cool": {"mode": "cool"},
+                    "pump": {"mode": "waterpump"},
+                },
+                "target_temperature": "set_temperature",
+                "current_temperature": "twout_temp",
+                "min_temp": "t5s_min",
+                "max_temp": "t5s_max",
+                "temperature_unit": UnitOfTemperature.CELSIUS,
+                "precision": 1,
+            }
+        },
+        Platform.SWITCH: {
+            "force_heat": {
+                "device_class": SwitchDeviceClass.SWITCH,
+                "translation_key": "force_heat",
+            },
+            "mute_en": {
+                "device_class": SwitchDeviceClass.SWITCH,
+                "translation_key": "mute_en",
+            },
+        },
+        Platform.SENSOR: {
+            "mode": {
+                "device_class": SensorDeviceClass.ENUM,
+                "translation_key": "mode",
+            },
+            "set_temperature": {
+                "device_class": SensorDeviceClass.TEMPERATURE,
+                "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                "state_class": SensorStateClass.MEASUREMENT,
+                "translation_key": "set_temperature",
+            },
+            "twin_temp": {
+                "device_class": SensorDeviceClass.TEMPERATURE,
+                "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                "state_class": SensorStateClass.MEASUREMENT,
+                "translation_key": "inlet_water_temperature",
+            },
+            "twout_temp": {
+                "device_class": SensorDeviceClass.TEMPERATURE,
+                "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                "state_class": SensorStateClass.MEASUREMENT,
+                "translation_key": "outlet_water_temperature",
+            },
+            "t4_temp": {
+                "device_class": SensorDeviceClass.TEMPERATURE,
+                "unit_of_measurement": UnitOfTemperature.CELSIUS,
+                "state_class": SensorStateClass.MEASUREMENT,
+                "translation_key": "outside_temperature",
+            },
+            "total_kwh": {
+                "device_class": SensorDeviceClass.ENERGY,
+                "unit_of_measurement": UnitOfEnergy.KILO_WATT_HOUR,
+                "state_class": SensorStateClass.TOTAL_INCREASING,
+                "translation_key": "total_kwh",
+            },
+        },
+    },
+}
 
 DEVICE_MAPPING = {
     "default": {
@@ -73,5 +146,7 @@ DEVICE_MAPPING = {
                 }
             }
         }
-    }
+    },
+    "171000AU": _POOL_HEAT_PUMP_MAPPING,
+    ("subtype", "513"): _POOL_HEAT_PUMP_MAPPING,
 }
